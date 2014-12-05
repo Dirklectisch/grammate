6ce8b335d63cc02913abfb03c5218ab22ba4dad4
Dirk Geurs <dirklectisch@mac.com>
2014-12-05 21:45:19 +0100

    Prepare 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject grammate "0.1.0-SNAPSHOT"
+(defproject grammate "0.1.0"
   :description "A collection of tools for working with TextMate grammars."
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [fipp "0.5.1"]
                  [org.clojure/test.check "0.6.1"]]
